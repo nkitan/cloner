a7f809467456264eb3e851e7492f3a387c94b75c
Ankit Das <ankitdas@protonmail.com>
2019-08-05 19:23:03 +0530

    Update clonerinstall.sh

--- clonerinstall.sh
+++ clonerinstall.sh
@@ -3,3 +3,7 @@ cd cloner
 chmod 775 cloner.sh
 cd
 echo "alias clone='cd ~/cloner ; ./cloner.sh ; cd'" >> .bashrc
+cd
+source .bashrc
+echo "INSTALLED SUCCESSFULLY"
+echo "Type clone then hit enter to get started"
