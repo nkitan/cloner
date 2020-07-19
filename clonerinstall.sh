fa1a05fc8889c39563ec0e24e4ac043cf97f94b4
Ankit Das <ankitdas2k@gmail.com>
2020-07-19 20:49:20 +0530

    Update clonerinstall.sh

--- clonerinstall.sh
+++ clonerinstall.sh
@@ -1,7 +1,19 @@
 chmod +x cloner.sh
 sudo mv cloner.sh $HOME/cloner/
-echo "alias clone='cd $HOME/cloner ; ./cloner.sh ; cd'" >> $HOME/.bashrc
 
-source $HOME/.bashrc
-echo "INSTALLED SUCCESSFULLY"
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]
+ then                         
+     echo "alias clone='cd $HOME/cloner ; ./cloner.sh ; cd'" >> $HOME/.zshrc
+     echo "Installed Successfully on zsh"
+    
+ elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]
+ then
+      echo "alias clone='cd $HOME/cloner ; ./cloner.sh ; cd'" >> $HOME/.bashrc
+      source $HOME/.bashrc
+      echo "Installed Successfully on bash"
+        
+ else
+       echo "Unknown SHELL!!! try contacting https://github.com/nkitan "      # you can try adding your own sh's, feel free.
+ fi
+
 echo "Type clone then hit enter to get started"
